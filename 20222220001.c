14abcf3bc6991dc0b45b6b1bfa53c9b5f6dde62b
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2022-12-21 05:55:35 -0400

    Update 20222220001.c

--- 20222220001.c
+++ 20222220001.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main(){
 
+float soma;
+float numero;
 
-int main(int argc, char *argv[]) {
-	float n1, n2, n3, n4, n5, soma;
-	printf ("Entre com 5 numeros :");
-	scanf ( "\n %f %f %f %f %f", &n1, &n2, &n3, &n4, &n5);
-	soma = n1 + n2 + n3 + n4 + n5;
-	printf (" A soma de cinco numero: %.2f", soma);
-	return 0;
+for(int i = 1; i <=5 ; i++){
+
+    printf("Digite um valor a ser somado: ");
+    scanf("%f",&numero);
+
+    soma += numero;
+
+}
+
+printf("O resultado da soma dos valores e: %.2f",soma);
+
+return 0;
 }
